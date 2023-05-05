fdfa57031fd0fe46a053869822f58b51bd987a3d
tuankthai <ttlive4jc@gmail.com>
2023-05-05 12:43:06 -0700

    block 16 demo code

---
+++ demo.js
@@ -0,0 +1,29 @@
+function rectangleArea(length, height) {
+    // console.log(length*height)
+    return length*height
+    
+    
+}
+
+const total = (rectangleArea(10, 10) + rectangleArea(10, 5) )*2
+
+// function triangleArea(length, height) {
+//     console.log(0.5*length * height)
+
+// }
+
+// triangleArea(3, 5)
+
+console.log(total)
+console.log(total)
+
+
+//function expr
+// variable = function expr
+const rectangleAreaTwo = (length, height) => {
+    // console.log(length*height)
+    return length * height
+}
+
+//one liner function expr
+const rectangleAreaThree = (parm1, parm2) => length * height
